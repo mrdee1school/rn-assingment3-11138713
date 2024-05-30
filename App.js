543e5f27df46b46f5bc8d5a4fcb1b94cc06e5a6d
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2024-05-30 13:36:36 +0000

    2: Implementation of core components and search area

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { images } from "@/constants";
+import { images, icons } from "@/constants";
 
 export default function App() {
   return (
@@ -15,6 +15,36 @@ export default function App() {
           <Image source={images.profile} className="w-12 h-12 rounded-full" />
         </View>
       </View>
+
+      {/* Search area */}
+      <View className="flex-row space-x-8">
+        {/* Search box */}
+        <View className="bg-white px-6 py-3 text-base w-80 ml-6 rounded-2xl flex-row items-center">
+          <Image
+            source={icons.search}
+            className="w-5 h-5"
+            resizeMode="contain"
+            tintColor="#161622"
+          />
+          <TextInput
+            className="text-base ml-2 pb-1"
+            placeholder="Search"
+            placeholderTextColor="#161622"
+          />
+        </View>
+        {/* Search box */}
+
+        {/* Filter */}
+        <View className="pt-1 mr-20">
+          <Image
+            source={icons.filter}
+            className="w-9 h-9 rounded-lg p-3"
+            resizeMode="contain"
+          />
+        </View>
+        {/* Filter */}
+      </View>
+      {/* Search area */}
     </SafeAreaView>
   );
 }
